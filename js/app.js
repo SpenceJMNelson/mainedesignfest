59b28de263adc483fcf998bd10ca68812996e200
Spence Nelson <spenjmn@gmail.com>
2025-06-13 11:38:52 -0400

    scroll effects

--- js/app.js
+++ js/app.js
@@ -8,25 +8,4 @@ var animation = bodymovin.loadAnimation({
 
 
 
-(function() {
-  'use strict';
 
-  var section = document.querySelectorAll(".event-item");
-  var sections = {};
-  var i = 0;
-
-  Array.prototype.forEach.call(section, function(e) {
-    sections[e.id] = e.offsetTop;
-  });
-
-  window.onscroll = function() {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-
-    for (i in sections) {
-      if (sections[i] <= scrollPosition) {
-        document.querySelector('.active').setAttribute('class', ' ');
-        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
-      }
-    }
-  };
-})();
