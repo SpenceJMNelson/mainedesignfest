866591e87b494e2bc3c39451c48fd784c0f8ac4b
Spence Nelson <spenjmn@gmail.com>
2025-06-13 10:52:29 -0400

    test scroll functionality

--- js/app.js
+++ js/app.js
@@ -5,3 +5,28 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: 'df-animation2.json'
 })
+
+
+
+(function() {
+  'use strict';
+
+  var section = document.querySelectorAll(".event-item");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.active').setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+})();
