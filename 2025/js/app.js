1e337aeaa991c18cd09a04f6700d7a745a94d977
Spence Nelson <spenjmn@gmail.com>
2026-03-17 20:01:29 -0400

    changing to 2026

---
+++ 2025/js/app.js
@@ -0,0 +1,11 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'df-animation2.json'
+})
+
+
+
+
